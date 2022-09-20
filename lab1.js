6e42365c72d169ae3848b692fee2648009fd8c30
Haotian Yan <hyan227@uwo.ca>
2022-09-19 21:47:33 -0400

    Add two functions for two search bars to match images with input numers or names so that mathched images will be displayed and hide the others

--- lab1.js
+++ lab1.js
@@ -6,3 +6,38 @@ function onlyNumberKey(evt) {
         return false;
     return true;
 }
+function myFunction1() {
+    var searchText = document.getElementById("name").value;
+    var divs = document.querySelectorAll(".my-li > div");
+
+    if (searchText.length > 0) {
+        divs.forEach((div) => {
+            div.classList.add("hide");
+            if ((div.dataset.tags.toUpperCase()).indexOf(searchText.toUpperCase()) > -1) {
+                div.classList.remove("hide");
+            }
+        });
+    } else {
+        divs.forEach((div) => {
+            div.classList.remove("hide");
+        });
+    }
+}
+
+function myFunction2() {
+    var searchText = document.getElementById("number").value;
+    var divs = document.querySelectorAll(".my-li > div");
+
+    if (searchText.length > 0) {
+        divs.forEach((div) => {
+            div.classList.add("hide");
+            if ((div.dataset.tags.toUpperCase()).indexOf(searchText.toUpperCase()) > -1) {
+                div.classList.remove("hide");
+            }
+        });
+    } else {
+        divs.forEach((div) => {
+            div.classList.remove("hide");
+        });
+    }
+}
