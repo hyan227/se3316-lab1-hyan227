e6c81129ff298d178c863dbc3fdec6beb79707d9
Haotian Yan <hyan227@uwo.ca>
2022-09-19 21:37:12 -0400

    Add javascript file and limit the number search bar be only allowed for numbers

---
+++ lab1.js
@@ -0,0 +1,8 @@
+function onlyNumberKey(evt) {
+
+    // Only ASCII character in that range allowed
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
